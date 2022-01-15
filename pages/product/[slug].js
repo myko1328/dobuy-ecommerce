2f1bac56ef622caacd67254eb7045272a0beb812
mykomiparanum <m.miparanum@gmail.com>
2022-01-15 13:31:36 +0100

    update product slug template

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,19 +1,110 @@
-import { useRouter } from 'next/router'
-import React from 'react'
-import data from '../../utils/data'
+import { useRouter } from 'next/router';
+import React from 'react';
+import data from '../../utils/data';
+import Layout from '../../components/Layout';
+import NextLink from 'next/link';
+import {
+	Button,
+	Card,
+	Grid,
+	Link,
+	List,
+	ListItem,
+	Typography,
+} from '@material-ui/core';
+import useStyle from '../../utils/styles';
+import Image from 'next/image';
 
 const ProductScreen = () => {
-  const router = useRouter()
-  const { slug } = router.query
-  const product = data.products.find((a) => a.slug === slug)
-  if (!product) {
-    return <div>Product not found</div>
-  }
-  return (
-    <div>
-      <h1>{product.name}</h1>
-    </div>
-  )
-}
+	const router = useRouter();
+	const classes = useStyle();
+	const { slug } = router.query;
+	const product = data.products.find((a) => a.slug === slug);
+	if (!product) {
+		return <div>Product not found</div>;
+	}
+	return (
+		// <div>
+		// 	{!product ? <div>Product not found </div> : <h1>{product.name}</h1>}
+		// </div>
+		<Layout title={product.name} description={product.description}>
+			<div className={classes.section}>
+				<NextLink href='/' passHref>
+					<Link>back to products</Link>
+				</NextLink>
+			</div>
+			<Grid container spacing={1}>
+				<Grid item md={6} xs={12}>
+					<Image
+						src={product.image}
+						alt={product.name}
+						width={340}
+						height={340}
+						layout='responsive'
+					></Image>
+				</Grid>
+				<Grid item md={3} xs={12}>
+					<List>
+						<ListItem>
+							<Typography component='h1' variant='h1'>
+								{product.name}
+							</Typography>
+						</ListItem>
+						<ListItem>
+							{' '}
+							<Typography> Category: {product.category}</Typography>
+						</ListItem>
+						<ListItem>
+							{' '}
+							<Typography> Brand: {product.brand}</Typography>
+						</ListItem>
+						<ListItem>
+							<Typography>
+								Rating: {product.rating} stars ({product.numReviews} reviews)
+							</Typography>
+						</ListItem>
+						<ListItem>
+							<Typography> Description: {product.description}</Typography>
+						</ListItem>
+					</List>
+				</Grid>
+				<Grid item md={3} xs={12}>
+					<Card>
+						<List>
+							<ListItem>
+								<Grid container>
+									<Grid item xs={6}>
+										<Typography>Price: </Typography>
+									</Grid>
+									<Grid item xs={6}>
+										<Typography> ${product.price}</Typography>
+									</Grid>
+								</Grid>
+							</ListItem>
+							<ListItem>
+								<Grid container>
+									<Grid item xs={6}>
+										<Typography>Status </Typography>
+									</Grid>
+									<Grid item xs={6}>
+										<Typography>
+											{' '}
+											${product.countInStock > 0 ? 'In Stock' : 'Unavailable'}
+										</Typography>
+									</Grid>
+								</Grid>
+							</ListItem>
+							<ListItem>
+								<Button fullWidth variant='contained' color='primary'>
+									Add to cart
+								</Button>
+							</ListItem>
+						</List>
+					</Card>
+				</Grid>
+			</Grid>
+		</Layout>
+	);
+};
 
-export default ProductScreen
+export default ProductScreen;
