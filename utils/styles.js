555483a2f9eb4d2cc2d93a65d9f1f7d56e3ca4f9
mykomiparanum <m.miparanum@gmail.com>
2022-01-15 13:31:42 +0100

    added styles

--- utils/styles.js
+++ utils/styles.js
@@ -1,32 +1,37 @@
-import { makeStyles } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core';
 
 const useStyle = makeStyles({
-  navbar: {
-    backgroundColor: '#203040',
-    '& a': {
-      color: '#ffffff',
-      marginLeft: 10,
-    },
-  },
-  main: {
-    minHeight: '80vh',
-  },
-  brand: {
-    fontWeight: 'bold',
-    fontSize: '1.5rem',
-    color: 'white',
-  },
-  navLinks: {
-    fontWeight: 'bold',
-    fontSize: '1.5rem',
-    color: 'white',
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  footer: {
-    textAlign: 'center',
-  },
-})
+	navbar: {
+		'backgroundColor': '#203040',
+		'& a': {
+			color: '#ffffff',
+			marginLeft: 10,
+		},
+	},
+	main: {
+		minHeight: '80vh',
+	},
+	brand: {
+		fontWeight: 'bold',
+		fontSize: '1.5rem',
+		color: 'white',
+	},
+	navLinks: {
+		fontWeight: 'bold',
+		fontSize: '1.5rem',
+		color: 'white',
+	},
+	grow: {
+		flexGrow: 1,
+	},
+	footer: {
+		marginTop: 10,
+		textAlign: 'center',
+	},
+	section: {
+		marginTop: 10,
+		marginBottom: 10,
+	},
+});
 
-export default useStyle
+export default useStyle;
