cd193d90bf49f7980ee54a70f7d1734afa33cd67
mykomiparanum <m.miparanum@gmail.com>
2022-01-22 13:03:44 +0100

    utility fnction to convert object from mongoose

--- utils/db.js
+++ utils/db.js
@@ -3,8 +3,6 @@ import mongoose from 'mongoose';
 const connection = {};
 
 async function connect() {
-	console.log(connection);
-
 	if (connection.isConnected) {
 		console.log('already connected');
 		return;
@@ -33,5 +31,13 @@ async function disconnect() {
 	}
 }
 
-const db = { connect, disconnect };
+function convertDocToObj(doc) {
+	doc._id = doc._id.toString();
+	doc.createdAt = doc.createdAt.toString();
+	doc.updatedAt = doc.updatedAt.toString();
+
+	return doc;
+}
+
+const db = { connect, disconnect, convertDocToObj };
 export default db;
