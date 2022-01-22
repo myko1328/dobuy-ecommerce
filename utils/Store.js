0dce2e9c651df6b288c42bbcb3d067bc7e6a9969
mykomiparanum <m.miparanum@gmail.com>
2022-01-22 14:00:16 +0100

    implement add to cart state

--- utils/Store.js
+++ utils/Store.js
@@ -9,6 +9,19 @@ function reducer(state, action) {
 			return { ...state, darkMode: true };
 		case 'DARK_MODE_OFF':
 			return { ...state, darkMode: false };
+		case 'CART_ADD_ITEM': {
+			const newItem = action.payload;
+			const existItem = state.cart.cartItems.find(
+				(item) => item.name === newItem.name
+			);
+			const cartItems = existItem
+				? state.cart.cartItems.map((item) =>
+						item._id === existItem.name ? newItem : item
+				  )
+				: [...state.cart.cartItems, newItem];
+			Cookies.set('cartItems', JSON.stringify(cartItems));
+			return { ...state, cart: { ...state.cart, cartItems } };
+		}
 		default:
 			return state;
 	}
@@ -17,6 +30,11 @@ function reducer(state, action) {
 export function StoreProvider(props) {
 	const [state, dispatch] = useReducer(reducer, {
 		darkMode: Cookies.get('darkMode') === 'ON' ? true : false,
+		cart: {
+			cartItems: Cookies.get('cartItems')
+				? JSON.parse(Cookies.get('cartItems'))
+				: [],
+		},
 	});
 
 	const value = { state, dispatch };
