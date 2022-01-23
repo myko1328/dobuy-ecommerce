4423b586d5e2878989fc28d548177436460b0395
mykomiparanum <m.miparanum@gmail.com>
2022-01-23 18:37:36 +0100

    implement redirect if not registered

--- pages/shipping.js
+++ pages/shipping.js
@@ -1,10 +1,16 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useContext } from 'react';
+import { Store } from '../utils/Store';
 
 const Shipping = () => {
 	const router = useRouter();
-	router.push('/login');
-	return <div></div>;
+	const { state, dispatch } = useContext(Store);
+	const { userInfo } = state;
+	if (!userInfo) {
+		router.push('/login?redirect=/shipping');
+	}
+
+	return <div>Shipping</div>;
 };
 
 export default Shipping;
