7c6d28c1f0c85be836d7cb0bbb4078881bf16b29
mykomiparanum <m.miparanum@gmail.com>
2022-01-23 19:01:53 +0100

    implement register layout and api

---
+++ pages/register.js
@@ -0,0 +1,121 @@
+import {
+	Button,
+	Link,
+	List,
+	ListItem,
+	TextField,
+	Typography,
+} from '@material-ui/core';
+import React, { useContext, useEffect, useState } from 'react';
+import Layout from '../components/Layout';
+import NextLink from 'next/link';
+import useStyle from '../utils/styles';
+import axios from 'axios';
+import { Store } from '../utils/Store';
+import { useRouter } from 'next/router';
+import Cookies from 'js-cookie';
+
+const Register = () => {
+	const router = useRouter();
+	const { redirect } = router.query; //login?redirect=shipping
+	const { state, dispatch } = useContext(Store);
+	const { userInfo } = state;
+
+	useEffect(() => {
+		if (userInfo) {
+			router.push('/');
+		}
+	}, []);
+
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+
+	const classes = useStyle();
+
+	const submitHandler = async (e) => {
+		e.preventDefault();
+		if (password !== confirmPassword) {
+			alert("password don't match");
+			return;
+		}
+		try {
+			const { data } = await axios.post('/api/users/register', {
+				name,
+				email,
+				password,
+			});
+			dispatch({ type: 'USER_LOGIN', payload: data });
+			Cookies.set('userInfo', JSON.stringify(data));
+			router.push(redirect || '/');
+		} catch (error) {
+			alert(error.response.data ? error.response.data.message : error.message);
+		}
+	};
+
+	return (
+		<Layout title='Register'>
+			<form onSubmit={submitHandler} className={classes.form}>
+				<Typography component='h1' variant='h1'>
+					Register
+				</Typography>
+				<List>
+					<ListItem>
+						<TextField
+							variant='outlined'
+							fullWidth
+							id='name'
+							label='Name'
+							inputProps={{ type: 'name' }}
+							onChange={(e) => setName(e.target.value)}
+						></TextField>
+					</ListItem>
+					<ListItem>
+						<TextField
+							variant='outlined'
+							fullWidth
+							id='email'
+							label='Email'
+							inputProps={{ type: 'email' }}
+							onChange={(e) => setEmail(e.target.value)}
+						></TextField>
+					</ListItem>
+					<ListItem>
+						<TextField
+							variant='outlined'
+							fullWidth
+							id='password'
+							label='Password'
+							inputProps={{ type: 'password' }}
+							onChange={(e) => setPassword(e.target.value)}
+						></TextField>
+					</ListItem>
+					<ListItem>
+						<TextField
+							variant='outlined'
+							fullWidth
+							id='confirmPassword'
+							label='Confirm Password'
+							inputProps={{ type: 'password' }}
+							onChange={(e) => setConfirmPassword(e.target.value)}
+						></TextField>
+					</ListItem>
+					<ListItem>
+						<Button variant='contained' type='submit' fullWidth color='primary'>
+							Register
+						</Button>
+					</ListItem>
+					<ListItem>
+						Already have an account? &nbsp;
+						<NextLink href={`/login?redirect=${redirect || '/'}`} passHref>
+							<Link>Login</Link>
+						</NextLink>
+					</ListItem>
+				</List>
+			</form>
+		</Layout>
+	);
+};
+
+export default Register;
