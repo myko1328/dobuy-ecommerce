fff26d0e123c83cda890e9b3e4fafdb5fad34c1a
mykomiparanum <m.miparanum@gmail.com>
2022-01-23 18:33:32 +0100

    implement login api

--- pages/login.js
+++ pages/login.js
@@ -6,16 +6,50 @@ import {
 	TextField,
 	Typography,
 } from '@material-ui/core';
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import NextLink from 'next/link';
 import useStyle from '../utils/styles';
+import axios from 'axios';
+import { Store } from '../utils/Store';
+import { useRouter } from 'next/router';
+import Cookies from 'js-cookie';
 
 const Login = () => {
+	const router = useRouter();
+	const { redirect } = router.query; //login?redirect=shipping
+	const { state, dispatch } = useContext(Store);
+	const { userInfo } = state;
+
+	useEffect(() => {
+		if (userInfo) {
+			router.push('/');
+		}
+	}, []);
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
 	const classes = useStyle();
+
+	const submitHandler = async (e) => {
+		e.preventDefault();
+
+		try {
+			const { data } = await axios.post('/api/users/login', {
+				email,
+				password,
+			});
+			dispatch({ type: 'USER_LOGIN', payload: data });
+			Cookies.set('userInfo', JSON.stringify(data));
+			router.push(redirect || '/');
+		} catch (error) {
+			alert(error.response.data ? error.response.data.message : error.message);
+		}
+	};
+
 	return (
 		<Layout title='login'>
-			<form className={classes.form}>
+			<form onSubmit={submitHandler} className={classes.form}>
 				<Typography component='h1' variant='h1'>
 					Login
 				</Typography>
@@ -27,6 +61,7 @@ const Login = () => {
 							id='email'
 							label='Email'
 							inputProps={{ type: 'email' }}
+							onChange={(e) => setEmail(e.target.value)}
 						></TextField>
 					</ListItem>
 					<ListItem>
@@ -36,6 +71,7 @@ const Login = () => {
 							id='password'
 							label='password'
 							inputProps={{ type: 'password' }}
+							onChange={(e) => setPassword(e.target.value)}
 						></TextField>
 					</ListItem>
 					<ListItem>
